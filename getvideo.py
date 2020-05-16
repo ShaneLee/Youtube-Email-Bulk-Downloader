58070ff41cf59f828f64b5f06732dad3f5ce1a97
Shane Lee <me@shanel.ee>
2020-05-16 13:00:06 +0100

    Repeat on no videos found - issue #2

--- getvideo.py
+++ getvideo.py
@@ -1,9 +1,16 @@
 import pytube
 import yt_email
+import time 
 
-links = yt_email.get_video_links()
+def getvideos():
+    links = yt_email.get_video_links()
+    if links == []: 
+        time.sleep(60)
+        getvideos()
 
-for link in links:
-    print(link)
-    pytube.YouTube(link).streams.first().download('.')
-    
+    for link in links:
+        print(link)
+        pytube.YouTube(link).streams.first().download('.')
+        
+if __name__ == '__main__':
+    getvideos()
